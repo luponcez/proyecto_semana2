d1e39d6db8785fd28a7990b74308c27d72994e06
Luis Ponce <luponcez@duocuc.cl>
2025-03-24 22:18:33 -0300

    id peliculas

--- src/main/java/com/duoc/proyecto_semana2/MoviesController.java
+++ src/main/java/com/duoc/proyecto_semana2/MoviesController.java
@@ -15,8 +15,8 @@ public class MoviesController {
         movies.add(new Movie(1, "Shrek", 2001, "Andrew Adamson", "Animación", "Un ogro llamado Shrek emprende una misión para rescatar a una princesa."));
         movies.add(new Movie(2, "Megamente", 2010, "Tom McGrath", "Animación", "Un supervillano descubre su propósito cuando finalmente derrota a su rival."));
         movies.add(new Movie(3, "El Rey León", 1994, "Roger Allers", "Animación", "Simba, un joven león, lucha por reclamar su lugar como rey."));
-        movies.add(new Movie(6, "Buscando a Nemo", 2003, "Andrew Stanton", "Animación", "Un pez payaso cruza el océano para encontrar a su hijo perdido."));
-        movies.add(new Movie(7, "Bichos", 1998, "John Lasseter", "Animación", "Una hormiga busca ayuda para salvar a su colonia de unos saltamontes."));
+        movies.add(new Movie(4, "Buscando a Nemo", 2003, "Andrew Stanton", "Animación", "Un pez payaso cruza el océano para encontrar a su hijo perdido."));
+        movies.add(new Movie(5, "Bichos", 1998, "John Lasseter", "Animación", "Una hormiga busca ayuda para salvar a su colonia de unos saltamontes."));
     }
 
     @GetMapping
