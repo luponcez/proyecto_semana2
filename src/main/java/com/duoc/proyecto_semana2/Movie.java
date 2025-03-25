1bf52ad5da6ca8665defc56b1f620f07e20085ca
Luis Ponce <luponcez@duocuc.cl>
2025-03-24 22:14:37 -0300

    Movie.java y MovieController.java

---
+++ src/main/java/com/duoc/proyecto_semana2/Movie.java
@@ -0,0 +1,68 @@
+package com.duoc.proyecto_semana2;
+
+public class Movie {
+    private int id;
+    private String title;
+    private int year;
+    private String director;
+    private String genre;
+    private String synopsis;
+
+    public Movie(int id, String title, int year, String director, String genre, String synopsis) {
+        this.id = id;
+        this.title = title;
+        this.year = year;
+        this.director = director;
+        this.genre = genre;
+        this.synopsis = synopsis;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() { 
+        return title; 
+    }
+
+    public int getYear() { 
+        return year;
+    }
+
+    public String getDirector() { 
+        return director; 
+    }
+
+    public String getGenre() { 
+        return genre; 
+    }
+
+    public String getSynopsis() { 
+        return synopsis; 
+    }
+
+    public void setId(int id) { 
+        this.id = id; 
+    }
+
+    public void setTitle(String title) { 
+        this.title = title; 
+    }
+
+    public void setYear(int year) { 
+        this.year = year; 
+    }
+
+    public void setDirector(String director) { 
+        this.director = director; 
+    }
+
+    public void setGenre(String genre) { 
+        this.genre = genre; 
+    }
+
+    public void setSynopsis(String synopsis) { 
+        this.synopsis = synopsis; 
+    }
+    
+}
---
+++ src/main/java/com/duoc/proyecto_semana2/MoviesController.java
@@ -0,0 +1,36 @@
+package com.duoc.proyecto_semana2;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/peliculas")
+public class MoviesController {
+
+    private List<Movie> movies = new ArrayList<>();
+
+    public MoviesController() {
+        movies.add(new Movie(1, "Shrek", 2001, "Andrew Adamson", "Animación", "Un ogro llamado Shrek emprende una misión para rescatar a una princesa."));
+        movies.add(new Movie(2, "Megamente", 2010, "Tom McGrath", "Animación", "Un supervillano descubre su propósito cuando finalmente derrota a su rival."));
+        movies.add(new Movie(3, "El Rey León", 1994, "Roger Allers", "Animación", "Simba, un joven león, lucha por reclamar su lugar como rey."));
+        movies.add(new Movie(6, "Buscando a Nemo", 2003, "Andrew Stanton", "Animación", "Un pez payaso cruza el océano para encontrar a su hijo perdido."));
+        movies.add(new Movie(7, "Bichos", 1998, "John Lasseter", "Animación", "Una hormiga busca ayuda para salvar a su colonia de unos saltamontes."));
+    }
+
+    @GetMapping
+    public List<Movie> getAllMovies() {
+        return movies;
+    }
+
+    @GetMapping("/{id}")
+    public Movie getMovieById(@PathVariable int id) {
+        for (Movie movie : movies) {
+            if (movie.getId() == id) {
+                return movie;
+            }
+        }
+        return null; 
+    }
+}
