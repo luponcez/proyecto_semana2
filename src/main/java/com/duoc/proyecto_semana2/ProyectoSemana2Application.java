9a459ac374bfb77f3e97e58926ea25cb0c5367f1
Luis Ponce <luponcez@duocuc.cl>
2025-03-24 21:12:30 -0300

    Configuracion Inicial

---
+++ src/main/java/com/duoc/proyecto_semana2/ProyectoSemana2Application.java
@@ -0,0 +1,13 @@
+package com.duoc.proyecto_semana2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoSemana2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoSemana2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/duoc/proyecto_semana2/ProyectoSemana2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duoc.proyecto_semana2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoSemana2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
